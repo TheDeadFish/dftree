39f2b0df30a92a88eeb0e3052a2e8bed37691cac
TheDeadFish <michaelstamper1@gmail.com>
2020-04-14 19:30:00 +0000

    rb.h: de-macrofying, macro simplifacation

--- rb.h
+++ rb.h
@@ -3,7 +3,13 @@
 #define rb_node(a_type)							\
 struct {								\
     a_type *rbn_left;							\
-    a_type *rbn_right_red;						\
+    intptr_t rbn_right_red;						\
+    a_type* right_get() { return (a_type*) (rbn_right_red & -2LL); } \
+    void right_set(a_type* x) { rbn_right_red = (rbn_right_red&1) | (intptr_t)x; } \
+    intptr_t red_get() { return rbn_right_red & 1; } \
+    void color_set(bool x) { rbn_right_red = (rbn_right_red&-2LL) | (intptr_t)x; } \
+    void red_set() { rbn_right_red |= 1; } \
+    void black_set() { rbn_right_red &= -2LL; } \
 }
 
 /* Root structure. */
@@ -13,38 +19,21 @@ struct {								\
 }
 
 /* Left accessors. */
-#define rbtn_left_get(a_type, a_field, a_node)				\
-    ((a_node)->a_field.rbn_left)
-#define rbtn_left_set(a_type, a_field, a_node, a_left) do {		\
-    (a_node)->a_field.rbn_left = a_left;				\
-} while (0)
+#define rbtn_left_get(a_type, a_field, a_node) ((a_node)->a_field.rbn_left)
+#define rbtn_left_set(a_type, a_field, a_node, a_left) (a_node)->a_field.rbn_left = a_left;				\
 
 /* Right accessors. */
-#define rbtn_right_get(a_type, a_field, a_node)				\
-    ((a_type *) (((intptr_t) (a_node)->a_field.rbn_right_red)		\
-      & ((ssize_t)-2)))
-#define rbtn_right_set(a_type, a_field, a_node, a_right) do {		\
-    (a_node)->a_field.rbn_right_red = (a_type *) (((uintptr_t) a_right)	\
-      | (((uintptr_t) (a_node)->a_field.rbn_right_red) & ((size_t)1)));	\
-} while (0)
+#define rbtn_right_get(a_type, a_field, a_node) ((a_node)->a_field.right_get())
+#define rbtn_right_set(a_type, a_field, a_node, a_right) (a_node)->a_field.right_set(a_right);
 
 /* Color accessors. */
-#define rbtn_red_get(a_type, a_field, a_node)				\
-    ((bool) (((uintptr_t) (a_node)->a_field.rbn_right_red)		\
-      & ((size_t)1)))
-#define rbtn_color_set(a_type, a_field, a_node, a_red) do {		\
-    (a_node)->a_field.rbn_right_red = (a_type *) ((((intptr_t)		\
-      (a_node)->a_field.rbn_right_red) & ((ssize_t)-2))			\
-      | ((ssize_t)a_red));						\
-} while (0)
-#define rbtn_red_set(a_type, a_field, a_node) do {			\
-    (a_node)->a_field.rbn_right_red = (a_type *) (((uintptr_t)		\
-      (a_node)->a_field.rbn_right_red) | ((size_t)1));			\
-} while (0)
-#define rbtn_black_set(a_type, a_field, a_node) do {			\
-    (a_node)->a_field.rbn_right_red = (a_type *) (((intptr_t)		\
-      (a_node)->a_field.rbn_right_red) & ((ssize_t)-2));		\
-} while (0)
+#define rbtn_red_get(a_type, a_field, a_node)	\
+    (a_node)->a_field.red_get()
+    
+#define rbtn_color_set(a_type, a_field, a_node, a_red) (a_node)->a_field.color_set(a_red)						\
+
+#define rbtn_red_set(a_type, a_field, a_node) (a_node)->a_field.red_set()
+#define rbtn_black_set(a_type, a_field, a_node) (a_node)->a_field.black_set()
 
 /* Node initializer. */
 #define rbt_node_new(a_type, a_field, a_rbt, a_node) do {		\
